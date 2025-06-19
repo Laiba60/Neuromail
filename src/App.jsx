4a01fac38848112a39bb4e472081ff9d0cc3ca00
Laiba60 <laibasaeed026@gmail.com>
2025-06-19 21:05:29 +0500

    Set the Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SentEmail from "./Components/SentEmail";
 
 const App = () => {
   return (
-    <div className="">
-    
-    </div>
+    <Router>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<SentEmail />} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
-export default App
+export default App;
---
+++ src/Components/SentEmail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SentEmail = () => {
+  return (
+    <div className="bg-[#00FFEA]">
+      <div>
+Close
+      </div>
+    </div>
+  )
+}
+
+export default SentEmail
